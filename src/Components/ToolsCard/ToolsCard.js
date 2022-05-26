972f33e366cf10544df614174f02d9f7f54f2123
Ayon <ayonjodder177@gmail.com>
2022-05-26 23:56:36 +0600

    All done

--- src/Components/ToolsCard/ToolsCard.js
+++ src/Components/ToolsCard/ToolsCard.js
@@ -8,11 +8,11 @@ const ToolsCard = ({ tool, handlePurchase }) => {
             <figure><img src={image} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{name}</h2>
-                <p>{details}</p>
-                <p>Price: ${price}</p>
-                <p>Stock: {availableQuantity}</p>
+                <p className='text-sm text-gray-500'>{details}</p>
+                <p className='text-lg font-semibold'>Price: ${price}</p>
+                <p className='font-medium'>Stock: {availableQuantity}</p>
                 <div className="card-actions justify-end mb-5">
-                    <button onClick={() => handlePurchase(_id)} className="btn btn-primary w-full text-white">Buy Now</button>
+                    <button onClick={() => handlePurchase(_id)} className="text-white bg-gradient-to-r from-primary to-secondary border-2 border-secondary hover:border-2 hover:border-primary hover:bg-gradient hover:from-white hover:to-white hover:text-primary transition-all transition-duration:150ms w-full font-medium hover:font-medium px-5 py-2 rounded-md">Buy Now</button>
                 </div>
             </div>
         </div>
